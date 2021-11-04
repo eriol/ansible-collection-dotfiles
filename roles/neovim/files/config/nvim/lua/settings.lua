33bc8cdaa3dcbe74d5f23e414028ee3ce7cfdabf
Daniele Tricoli <eriol@mornie.org>
2021-11-04 03:47:39 +0100

    Add more options about tabs and UI

--- roles/neovim/files/config/nvim/lua/settings.lua
+++ roles/neovim/files/config/nvim/lua/settings.lua
@@ -12,7 +12,18 @@ opt.mouse = 'a'
 opt.colorcolumn = '80'
 opt.list = true
 opt.listchars = { eol = '¬', nbsp='␣', tab = '▸ ', trail = '·' }
+opt.showbreak = '↪'
+opt.signcolumn = 'yes'
+
+-- Tabs
+
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.smartindent = true
+opt.softtabstop = 4
+opt.tabstop = 4
 
 -- Colorscheme
+
 opt.termguicolors = true
 cmd [[colorscheme PaperColor]]
