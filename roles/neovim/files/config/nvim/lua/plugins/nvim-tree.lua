989cf69030ac6c496772183a76782c0417c7a0f1
Daniele Tricoli <eriol@mornie.org>
2021-11-08 03:24:38 +0100

    Add nvim-tree plugin

---
+++ roles/neovim/files/config/nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,13 @@
+require('nvim-tree').setup{
+    auto_close = true,
+    update_focused_file = {
+        enable = true,
+    },
+    view = {
+        width = 35
+    }
+}
+
+vim.g.nvim_tree_quit_on_open = 1
+
+vim.api.nvim_set_keymap("n", "<C-n>", "<cmd>NvimTreeToggle<cr>", {})
--- roles/neovim/files/config/nvim/lua/plugins/packer.lua
+++ roles/neovim/files/config/nvim/lua/plugins/packer.lua
@@ -26,6 +26,10 @@ return require('packer').startup(function()
         'nvim-telescope/telescope.nvim',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
+    use {
+        'kyazdani42/nvim-tree.lua',
+        requires = 'kyazdani42/nvim-web-devicons'
+    }
     use 'mhinz/vim-startify'
 
     use {
