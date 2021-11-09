31af5450e5f63471daa13bc6b655b31a8aebcb7b
Daniele Tricoli <eriol@mornie.org>
2021-11-09 19:50:39 +0100

    Remove the TODO since they are needed.

--- roles/neovim/files/config/nvim/lua/plugins/nvim-cmp.lua
+++ roles/neovim/files/config/nvim/lua/plugins/nvim-cmp.lua
@@ -4,7 +4,6 @@ cmp.setup({
     completion = {
         completeopt = 'menu,menuone,noinsert'
     },
-    -- TODO: S-Tab and Tab mappings seems that can be removed.
     mapping = {
         ['<cr>'] = cmp.mapping.confirm(),
         ["<S-Tab>"] = cmp.mapping.select_prev_item(),
