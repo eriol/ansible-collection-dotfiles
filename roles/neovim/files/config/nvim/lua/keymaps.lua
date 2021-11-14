5af0fdd717108161914813139f80e75de14af7e7
Daniele Tricoli <eriol@mornie.org>
2021-11-14 02:20:54 +0100

    Fix broken Page Up/Down

--- roles/neovim/files/config/nvim/lua/keymaps.lua
+++ roles/neovim/files/config/nvim/lua/keymaps.lua
@@ -2,7 +2,7 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 -- CTRL+l to clear search highlighting.
-map('n', '<C-l>', ':nohlsearch<cr>', opts)
+map('n', '<C-l>', ':nohlsearch<cr><C-l>', opts)
 
 -- Move into splits after creation.
 map('n', '<C-w>v', '<C-w>v<C-w>w', opts)
@@ -13,3 +13,10 @@ map('n', '<leader>s', ':w<cr>', opts)
 
 -- Close all and exit.
 map('n', '<leader>q', ':qa!<cr>', opts)
+
+-- Fix broken Page Up/Down.
+-- https://vimrc-dissection.blogspot.com/2009/02/fixing-pageup-and-pagedown.html
+map('n', '<PageUp>', '1000<C-u>', opts)
+map('n', '<PageDown>', '1000<C-d>', opts)
+map('i', '<PageUp>', '<C-o>1000<C-u>', opts)
+map('i', '<PageDown>', '<C-o>1000<C-d>', opts)
