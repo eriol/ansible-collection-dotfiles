78a336825971c996f74291ffb0a68149b0229e31
Daniele Tricoli <eriol@mornie.org>
2022-12-13 03:01:56 +0100

    Format with stylua

--- roles/neovim/files/config/nvim/lua/plugins/lspconfig.lua
+++ roles/neovim/files/config/nvim/lua/plugins/lspconfig.lua
@@ -70,13 +70,13 @@ null_ls.setup({
     },
     on_attach = function(client, bufnr)
         -- Format on save.
-        if client.supports_method("textDocument/formatting") then
+        if client.supports_method "textDocument/formatting" then
             vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
             vim.api.nvim_create_autocmd("BufWritePre", {
                 group = augroup,
                 buffer = bufnr,
                 callback = function()
-                   vim.lsp.buf.format({ bufnr = bufnr })
+                    vim.lsp.buf.format({ bufnr = bufnr })
                 end,
             })
         end
