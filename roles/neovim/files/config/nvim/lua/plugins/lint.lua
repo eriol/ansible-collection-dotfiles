ef89124a2606c1711c653b2379b93bb69e379550
Daniele Tricoli <eriol@mornie.org>
2023-09-15 17:21:18 +0200

    Use ansible-lint

--- roles/neovim/files/config/nvim/lua/plugins/lint.lua
+++ roles/neovim/files/config/nvim/lua/plugins/lint.lua
@@ -1,6 +1,6 @@
 require("lint").linters_by_ft = {
     python = { "flake8" },
-    yaml = { "yamllint" },
+    yaml = { "yamllint", "ansible_lint" },
 }
 
 vim.cmd [[ autocmd BufWritePost * lua require('lint').try_lint() ]]
