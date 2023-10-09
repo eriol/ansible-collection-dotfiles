7575573dc6b2cc7e6a729b951ffa500ca28cc12e
Daniele Tricoli <eriol@mornie.org>
2023-10-10 00:54:37 +0200

    No need for custom configuration

--- roles/neovim/files/config/nvim/lua/plugins/indent-blankline.lua
+++ roles/neovim/files/config/nvim/lua/plugins/indent-blankline.lua
@@ -1,10 +1,1 @@
-require("ibl").setup({
-    buftype_exclude = { "terminal" },
-    filetype_exclude = {
-        "help",
-        "packer",
-        "startify",
-    },
-    show_first_indent_level = false,
-    show_end_of_line = true,
-})
+require("ibl").setup()
