7559894c5381350521f8e272a84a86dc464dc000
Daniele Tricoli <eriol@mornie.org>
2024-03-19 00:19:15 +0100

    Don't pass the original file to the formatter

--- roles/neovim/files/config/nvim/lua/plugins/formatter.lua
+++ roles/neovim/files/config/nvim/lua/plugins/formatter.lua
@@ -26,7 +26,6 @@ require("formatter").setup({
                     exe = "dprint",
                     args = {
                         "fmt",
-                        util.escape_path(util.get_current_buffer_file_path()),
                     },
                     stdin = false,
                 }
