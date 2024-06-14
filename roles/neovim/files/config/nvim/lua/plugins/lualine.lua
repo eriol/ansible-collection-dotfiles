9f7127279fd767856e1e0be847cd1c6507457753
Daniele Tricoli <eriol@mornie.org>
2024-06-14 19:41:56 +0200

    Migrate lualine to lazy.nvim

--- roles/neovim/files/config/nvim/lua/plugins/lualine.lua
+++ roles/neovim/files/config/nvim/lua/plugins/lualine.lua
@@ -1,21 +1,29 @@
-require("lualine").setup({
-    options = {
-        icons_enabled = true,
-        theme = "auto",
-        component_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
-        disabled_filetypes = { "startify" },
-        always_divide_middle = true,
-        globalstatus = true,
+return {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
     },
-    sections = {
-        lualine_a = {
-            {
-                "mode",
-                fmt = function(str)
-                    return str:sub(1, 1)
-                end,
+    config = function()
+        require("lualine").setup({
+            options = {
+                icons_enabled = true,
+                theme = "auto",
+                component_separators = { left = "", right = "" },
+                section_separators = { left = "", right = "" },
+                disabled_filetypes = { "startify" },
+                always_divide_middle = true,
+                globalstatus = true,
             },
-        },
-    },
-})
+            sections = {
+                lualine_a = {
+                    {
+                        "mode",
+                        fmt = function(str)
+                            return str:sub(1, 1)
+                        end,
+                    },
+                },
+            },
+        })
+    end,
+}
