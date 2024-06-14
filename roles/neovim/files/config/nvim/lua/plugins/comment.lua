fbb812f489f770ff3cf41dfac00c51ef03f2a78f
Daniele Tricoli <eriol@mornie.org>
2024-06-14 19:40:45 +0200

    Mograte numToStr/Comment.nvim to lazy.nvim

--- roles/neovim/files/config/nvim/lua/plugins/comment.lua
+++ roles/neovim/files/config/nvim/lua/plugins/comment.lua
@@ -1,5 +1,10 @@
-require("Comment").setup()
-
-vim.api.nvim_set_keymap("n", "<C-_>", "gcc", {})
-vim.api.nvim_set_keymap("v", "<C-_>", "gc", {})
-vim.api.nvim_set_keymap("i", "<C-_>", "<esc>gcc i", {})
+return {
+    "numToStr/Comment.nvim",
+    lazy = false,
+    config = function()
+        require("Comment").setup()
+        vim.api.nvim_set_keymap("n", "<C-_>", "gcc", {})
+        vim.api.nvim_set_keymap("v", "<C-_>", "gc", {})
+        vim.api.nvim_set_keymap("i", "<C-_>", "<esc>gcc i", {})
+    end,
+}
