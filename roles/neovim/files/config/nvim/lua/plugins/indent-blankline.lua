133384255c98371012609405215eb833c4b80362
Daniele Tricoli <eriol@mornie.org>
2024-06-14 20:01:54 +0200

    Move indent-blankline to lazy.nvim

--- roles/neovim/files/config/nvim/lua/plugins/indent-blankline.lua
+++ roles/neovim/files/config/nvim/lua/plugins/indent-blankline.lua
@@ -1,5 +1,11 @@
-require("ibl").setup()
-
-require("ibl").update({
-    exclude = { filetypes = { "startify" } },
-})
+return {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {},
+    config = function()
+        --  TODO: this should be moved into opts
+        require("ibl").update({
+            exclude = { filetypes = { "startify" } },
+        })
+    end,
+}
