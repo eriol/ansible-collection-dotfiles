a5fb860fa1f199c039c5f5aff93bc95e1f2fd9c0
Daniele Tricoli <eriol@mornie.org>
2024-06-14 20:18:41 +0200

    Migrate lint to lazy.nvim

--- roles/neovim/files/config/nvim/lua/plugins/lint.lua
+++ roles/neovim/files/config/nvim/lua/plugins/lint.lua
@@ -1,6 +1,12 @@
-require("lint").linters_by_ft = {
-    python = { "flake8" },
-    yaml = { "yamllint", "ansible_lint" },
-}
+return {
+    "mfussenegger/nvim-lint",
+    config = function()
+        require("lint").linters_by_ft = {
+            python = { "flake8" },
+            yaml = { "yamllint", "ansible_lint" },
+        }
 
-vim.cmd [[ autocmd BufWritePost * lua require('lint').try_lint() ]]
+        -- TODO: https://github.com/mfussenegger/nvim-lint/issues/600#issuecomment-2168415996
+        vim.cmd [[ autocmd BufWritePost * lua require('lint').try_lint() ]]
+    end,
+}
