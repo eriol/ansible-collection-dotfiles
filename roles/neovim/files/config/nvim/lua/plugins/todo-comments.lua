fc19a7be8591e8b9423f616c8389dc0b37fc030f
Daniele Tricoli <eriol@mornie.org>
2024-06-18 12:08:44 +0200

    Load on BufReadPost, BufWritePost or BufNewFile

--- roles/neovim/files/config/nvim/lua/plugins/todo-comments.lua
+++ roles/neovim/files/config/nvim/lua/plugins/todo-comments.lua
@@ -1,6 +1,7 @@
 return {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
+    event = { "BufReadPost", "BufWritePost", "BufNewFile" },
     opts = {},
     keys = {
         { "<leader>xt", "<cmd>TodoTelescope<cr>" },
