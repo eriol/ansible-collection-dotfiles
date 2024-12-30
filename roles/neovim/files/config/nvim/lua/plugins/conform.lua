c5d06f0075759700d5945a822d8588e25d946680
Daniele Tricoli <eriol@mornie.org>
2024-12-30 02:25:12 +0100

    Switch to conform

---
+++ roles/neovim/files/config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,35 @@
+return {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+        {
+            "<leader>f",
+            function()
+                require("conform").format({ async = true })
+            end,
+            mode = "",
+            desc = "Format buffer",
+        },
+    },
+    opts = {
+        formatters_by_ft = {
+            cpp = { "clang-format" },
+            css = { "dprint" },
+            go = { "gofmt", "goimports" },
+            html = { "dprint" },
+            javascript = { "dprint", "prettierd", "prettier", stop_after_first = true },
+            lua = { "stylua" },
+            python = { "isort", "black" },
+            rust = { "rustfmt", lsp_format = "fallback" },
+            yaml = { "dprint" },
+        },
+        default_format_opts = {
+            lsp_format = "fallback",
+        },
+        format_on_save = { timeout_ms = 500, lsp_fallback = true },
+    },
+    init = function()
+        vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
+}
